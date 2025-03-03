c25bec4add3cc06367b3eefeb5dc3a6a8359f768
layu <xuruili@kugou.net>
2025-03-03 23:58:15 +0800

    fix

--- config/db.go
+++ config/db.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	// dsn := "root:123456@tcp(192.168.3.19:3306)/hope?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "root:123456@tcp(192.168.3.10:3306)/hope?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/hope?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
